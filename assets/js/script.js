95080bb07541117158919e9a7fa7e4807a79588f
AlvaLind <>
2024-01-05 21:46:33 +0000

    added score and moves tallies work using javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,19 +17,41 @@ let timeoutValue = 600;
  */
 function shuffleCards(array) {
     for (let i = array.length - 1; i > 0; i--) {                  //A for loop that runs through for each element in the array//
-        let x = Math.floor(Math.random() * (i + 1));            //Create x and assign it a random value to index between 0 and the current index//
+        const x = Math.floor(Math.random() * (i + 1));          //Create x and assign it a random value to index between 0 and the current index//
         [array[i], array[x]] = [array[x], array[i]];            //Swap the elements at index i and x in the array//
     }
     return array;                                               //returns the shuffled array// 
 }
 
-//Shuffle the cards array via the shuffleCards function//
-let shuffled_cards = shuffleCards(cards);
+const shuffled_cards = shuffleCards(cards);     //Shuffle the cards array via the shuffleCards function// 
 
-//Function that resets the game when the restart button is clicked//
+
+//Function to increment user score everytime they get a pair//
+function updateScore() {
+    score++;
+    document.getElementById('score').innerText = score;
+}
+
+//Function to increment users total number of moves each time they select two cards//
+function updateMoves() {
+    moves++;
+    document.getElementById('moves').innerText = moves;
+}
+
+//Function to reset the users score and moves to 0 if they restart the game//
+function resetTallies() {
+    score = 0;
+    moves = 0;
+    document.getElementById('score').innerText = score;
+    document.getElementById('moves').innerText = moves;
+}
+
+//Function that resets the game and the users score and moves tally - to be run when the restart button is clicked//
 function restartGame() {
+    resetTallies();
     window.location.reload();
 }
+
 
 //For loop to create a div (card) for each of the array values// 
 for (var i = 0; i < shuffled_cards.length; i++) {
@@ -40,19 +62,17 @@ for (var i = 0; i < shuffled_cards.length; i++) {
     cardImage.src = 'images/' + shuffled_cards[i];
     card.appendChild(cardImage);
 
-
     card.onclick = function () {            //When a card is clicked run the below//
-        if (!this.classList.contains('Match')) { // Check if the card is not already matched//
+        if (!this.classList.contains('Match')) { // Check if the card is not already matched
             this.classList.add('revealCard');
             setTimeout(function () {
                 const revealedCards = document.querySelectorAll('.revealCard');
 
-                //identify the users selected two cards from the array//
                 if (revealedCards.length > 1) {
                     const firstCardContent = revealedCards[0].innerHTML;
                     const secondCardContent = revealedCards[1].innerHTML;
 
-                    // If the cards two cards revealed or selected are a match leave them revealed and remove from cards array // 
+                    // If the cards two cards revealed or selected are a match // 
                     if (firstCardContent === secondCardContent) {
                         revealedCards[0].classList.add('Match');
                         revealedCards[1].classList.add('Match');
@@ -60,15 +80,17 @@ for (var i = 0; i < shuffled_cards.length; i++) {
                         revealedCards[0].classList.remove('revealCard');
                         revealedCards[1].classList.remove('revealCard');
 
-                        //If all the cards are matched display the winner alert//
+                        updateScore();
+
                         if (document.querySelectorAll('.Match').length === cards.length) {
                             alert('Winner!');
                         }
                     } else {
-                        // If cards do not match, turn the cards back around//
+                        // If cards do not match, remove 'revealCard' class
                         revealedCards[0].classList.remove('revealCard');
                         revealedCards[1].classList.remove('revealCard');
                     }
+                    updateMoves();
                 }
             }, timeoutValue); // Adjust the timeout value as needed
         }
