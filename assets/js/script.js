d55387198ff0f433962718e18dd364dea8ef910e
AlvaLind <>
2024-01-06 10:07:34 +0000

    fixed images links, added instructions and touched up css

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ const cards = ["lightning.png", "lightning.png", "cat.png", "cat.png", "bat.png"
  */
 let score = 0;
 let moves = 0;
-//To be used as a pause time between moves//
+//To be used as a pause time between moves
 let timeoutValue = 600;
 
 /** 
@@ -16,29 +16,35 @@ let timeoutValue = 600;
  * and returns the shuffled array in a random order.
  */
 function shuffleCards(array) {
-    for (let i = array.length - 1; i > 0; i--) {                  //A for loop that runs through for each element in the array//
-        const x = Math.floor(Math.random() * (i + 1));          //Create x and assign it a random value to index between 0 and the current index//
-        [array[i], array[x]] = [array[x], array[i]];            //Swap the elements at index i and x in the array//
+    for (let i = array.length - 1; i > 0; i--) {            //A for loop that runs through for each element in the array
+        const x = Math.floor(Math.random() * (i + 1));      //Create x and assign it a random value to index between 0 and the current index
+        [array[i], array[x]] = [array[x], array[i]];        //Swap the elements at index i and x in the array
     }
-    return array;                                               //returns the shuffled array// 
+    return array;                                //returns the shuffled array
 }
 
-const shuffled_cards = shuffleCards(cards);     //Shuffle the cards array via the shuffleCards function// 
+const shuffled_cards = shuffleCards(cards);     //Shuffle the cards array via the shuffleCards function
 
 
-//Function to increment user score everytime they get a pair//
+/**
+ * Function to increment user score everytime they get a pair
+ */
 function updateScore() {
     score++;
     document.getElementById('score').innerText = score;
 }
 
-//Function to increment users total number of moves each time they select two cards//
+/**
+ * Function to increment users total number of moves each time they select two cards
+ */
 function updateMoves() {
     moves++;
     document.getElementById('moves').innerText = moves;
 }
 
-//Function to reset the users score and moves to 0 if they restart the game//
+/**
+ * Function to reset the users score and moves to 0 if they restart the game
+ */
 function resetTallies() {
     score = 0;
     moves = 0;
@@ -46,23 +52,28 @@ function resetTallies() {
     document.getElementById('moves').innerText = moves;
 }
 
-//Function that resets the game and the users score and moves tally - to be run when the restart button is clicked//
+/**
+ * Function that resets the game and the users score 
+ * and moves tally - to be run when the restart button is clicked
+ */
 function restartGame() {
     resetTallies();
     window.location.reload();
 }
 
 
-//For loop to create a div (card) for each of the array values// 
+/**
+ * For loop to create a div (card) for each of the array values
+ */ 
 for (var i = 0; i < shuffled_cards.length; i++) {
     let card = document.createElement('div');
     card.className = 'card_box';
 
     let cardImage = document.createElement('img');
-    cardImage.src = 'images/' + shuffled_cards[i];
+    cardImage.src = 'assets/images/' + shuffled_cards[i];
     card.appendChild(cardImage);
 
-    card.onclick = function () {            //When a card is clicked run the below//
+    card.onclick = function () {            //When a card is clicked run the below
         if (!this.classList.contains('Match')) { // Check if the card is not already matched
             this.classList.add('revealCard');
             setTimeout(function () {
@@ -72,7 +83,7 @@ for (var i = 0; i < shuffled_cards.length; i++) {
                     const firstCardContent = revealedCards[0].innerHTML;
                     const secondCardContent = revealedCards[1].innerHTML;
 
-                    // If the cards two cards revealed or selected are a match // 
+                    // If the cards two cards revealed or selected are a match 
                     if (firstCardContent === secondCardContent) {
                         revealedCards[0].classList.add('Match');
                         revealedCards[1].classList.add('Match');
@@ -81,16 +92,17 @@ for (var i = 0; i < shuffled_cards.length; i++) {
                         revealedCards[1].classList.remove('revealCard');
 
                         updateScore();
+                        updateMoves();
 
                         if (document.querySelectorAll('.Match').length === cards.length) {
-                            alert('Winner!');
+                            alert(`CONGRATULATIONS YOU WON! You got all pairs in ${moves} moves!:D`);
                         }
                     } else {
                         // If cards do not match, remove 'revealCard' class
                         revealedCards[0].classList.remove('revealCard');
                         revealedCards[1].classList.remove('revealCard');
+                        updateMoves();
                     }
-                    updateMoves();
                 }
             }, timeoutValue); // Adjust the timeout value as needed
         }
